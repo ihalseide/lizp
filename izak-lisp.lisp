b8b8a7d00feba0b47f9bdcbc54bdbadeea241e44
Izak Halseide <ihalseide@pm.me>
2022-01-20 16:34:24 -0700

    Add #t, #f, #nil, do, and if
    The special symbol #T means true, #F means false, and #NIL means nil.
    DO is like PROGN in common lisp.
    IF is the same as IF in common lisp.

--- izak-lisp.lisp
+++ izak-lisp.lisp
@@ -1,6 +1,14 @@
 ;;;; Izak Lisp language
 ;;;; Tokenizer works
 
+(defun i-symbol? (x) (symbolp x))
+
+(defun i-list? (x) (listp x))
+
+(defun i-list-empty? (x) (null x))
+
+(defun i-integer? (x) (integerp x))
+
 (defun char-bracket? (c)
   (member c (list #\( #\) #\{ #\} #\[ #\]) :test #'char=))
 
@@ -88,17 +96,37 @@
       (first token))))
 
 (defun token->i-symbol (token)
-  (intern (coerce token 'string)))
+  (intern (string-upcase (coerce token 'string))))
 
 (defun token->i-integer (token)
   (parse-integer (coerce token 'string)))
 
+(defun token= (t1 t2)
+  (or (and (null t1)
+           (null t2))
+      (and (listp t1)
+           (listp t2)
+           (eql (first t1)
+                (first t2))
+           (token= (rest t1)
+                   (rest t2)))))
+
 (defun read-atom (reader) ; => form
   (let ((token (peek reader)))
     (when token
-      (if (digit-char-p (first token))
-        (token->i-integer (next reader))
-        (token->i-symbol (next reader))))))
+      (cond ((token= token (list #\# #\n #\i #\l))
+             (next reader)
+             :i-nil)
+            ((token= token (list #\# #\t))
+             (next reader)
+             :i-true)
+            ((token= token (list #\# #\f))
+             (next reader)
+             :i-false)
+            ((digit-char-p (first token))
+             (token->i-integer (next reader)))
+            (t
+              (token->i-symbol (next reader)))))))
 
 (defun read-list (reader) ; => form
   (let ((opener (first (next reader))) ; consume the opening bracket
@@ -126,12 +154,13 @@
 
 (defun print-to-stream (x stream)
   (cond ((null x)
-         ;; Empty list
          (princ "()" stream))
-        ((and (listp x)
-              (functionp (rest x)))
-         ;; Function/lambda
-         (format stream "<function `~a'>" (first x)))
+        ((eql x :i-nil)
+         (princ "#nil" stream))
+        ((eql x :i-true)
+         (princ "#t" stream))
+        ((eql x :i-false)
+         (princ "#f" stream))
         ((listp x)
          ;; Normal list
          (princ #\( stream)
@@ -142,23 +171,19 @@
          (princ #\) stream))
         (t
           ;; Symbol or number
-          (princ x stream))))
+          (princ x stream)))
+  stream)
 
 (defun print-string (obj)
-  (let ((str (make-string-output-stream)))
-    (print-to-stream obj str)
-    (get-output-stream-string str)))
-
-(defun i-symbol? (x) (symbolp x))
-
-(defun i-list? (x) (listp x))
-
-(defun i-list-empty? (x) (null x))
-
-(defun i-integer? (x) (integerp x))
+  (get-output-stream-string
+    (print-to-stream obj
+                     (make-string-output-stream))))
 
 (defun eval-ast (ast env)
-  (cond ((i-symbol? ast)
+  (cond ((eql ast :i-nil) ast)
+        ((eql ast :i-true) ast)
+        ((eql ast :i-false) ast)
+        ((i-symbol? ast)
          (env-get env ast))
         ((i-list? ast)
          (mapcar #'(lambda (x) (i-eval x env)) ast))
@@ -198,6 +223,11 @@
 (defun i-read (x)
   (read-str x))
 
+(defun first-symbol= (ast name)
+  (when (i-symbol? (first ast))
+    (string-equal (symbol-name (first ast))
+                  name)))
+
 (defun i-eval (ast env)
   (cond ((not (i-list? ast))
          ;; Atom
@@ -205,7 +235,7 @@
         ((i-list-empty? ast)
          ;; Empty list
          ast)
-        ((string-equal (symbol-name (first ast)) "def!")
+        ((first-symbol= ast "def!")
          ;; DEF! special form (def! <name> <expr>)
          (when (/= 3 (length ast))
            (error "`def!` form must have a length of 3"))
@@ -214,7 +244,7 @@
          (env-set env
                   (second ast)
                   (i-eval (third ast) env)))
-        ((string-equal (symbol-name (first ast)) "let*")
+        ((first-symbol= ast "let*")
          ;; LET* special form (let* <pairs> <expr>)
          (when (/= 3 (length ast))
            (error "`let*` form must have a length of 3"))
@@ -231,6 +261,26 @@
                                 (i-eval (second sublist) let-env))))
                      (second ast))
            (i-eval (third ast) let-env)))
+        ;; FUN* special form for functions
+        ((first-symbol= ast "fun*")
+         (error "not implemented yet"))
+        ;; DO special form (like PROGN)
+        ((first-symbol= ast "do")
+         (let ((latest nil))
+           (dolist (subexpr (rest ast) latest)
+             (setf latest
+                   (i-eval subexpr env)))))
+        ;; IF special form
+        ((first-symbol= ast "if")
+         (let ((condition-val (i-eval (second ast) env)))
+           (when (not (<= 3 (length ast) 4))
+             (error "special form `if' must have 2 or 3 forms after the `if'"))
+           (if (not (or (eql condition-val :i-nil)
+                        (eql condition-val :i-false)))
+             (i-eval (third ast) env)
+             (if (fourth ast)
+               (i-eval (fourth ast) env)
+               :i-nil))))
         (t
           ;; Normal list form
           (eval-call (eval-ast ast env)))))
@@ -239,15 +289,31 @@
   (print-string x))
 
 (defun rep (env)
-  (i-print (i-eval (i-read (read-line))
-                   env)))
+  (i-print
+    (i-eval (i-read (read-line))
+            env)))
+
+(defun ->i-bool (x)
+  (if x
+    :i-true
+    :i-false))
 
 (defun mainloop ()
   (let ((repl-env (make-environment nil)))
+    ;; Add symbols to the environment
     (env-set repl-env '- (lambda (x y) (- x y)))
     (env-set repl-env '+ (lambda (x y) (+ x y)))
     (env-set repl-env '* (lambda (x y) (* x y)))
     (env-set repl-env '/ (lambda (x y) (/ x y)))
+    (env-set repl-env 'sum (lambda (l) (apply #'+ l)))
+    (env-set repl-env 'list (lambda (&rest args) (apply #'list args)))
+    (env-set repl-env 'list? (lambda (x) (->i-bool (listp x))))
+    (env-set repl-env 'empty? (lambda (x) (->i-bool (null x))))
+    (env-set repl-env 'quit (lambda () (return-from mainloop)))
+
+    (format t "~&repl-env = ~s~&" repl-env)
+
+    ;; Start the loop
     (loop
       (princ "user> ")
       (princ (rep repl-env))
