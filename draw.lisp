b5063226af2ff2237c9c1f5d97d2822c22f64daf
Izak Halseide <ihalseide@pm.me>
2022-01-21 22:09:28 -0700

    change draw.lisp (broken)

--- draw.lisp
+++ draw.lisp
@@ -1,103 +1,132 @@
 ;(ql:quickload :cl-raylib)
 
-(defparameter *thing-margin* 5)
+(defparameter *font-size* 20)
 
 (defparameter *thing-id* 0)
-(defun make-thing (content &optional x y vertical)
-  (list :content content
-		:x x
-		:y y
-		:width nil   ; updated by drawing
-		:height nil  ; updated by drawing
-		:vertical vertical
-		:id (incf *thing-id*)))
-(defun thing-content (thing) (getf thing :content))
-(defun thing-x (thing) (getf thing :x))
-(defun thing-y (thing) (getf thing :y))
-(defun thing-width (thing) (getf thing :width))
-(defun thing-height (thing) (getf thing :height))
-(defun thing-vertical (thing) (getf thing :vertical))
-(defun thing-id (thing) (getf thing :id))
-(defun thing= (t1 t2) (= (thing-id t1) (thing-id t2)))
-(defun thing-update-pos (thing x y)
-  (setf (getf thing :x) x)
-  (setf (getf thing :y) y))
-(defun thing-update-size (thing width height)
-  (setf (getf thing :width) width)
-  (setf (getf thing :height) height))
 
-(defun make-size (w h) (cons w h))
-(defun width (size) (first size))
-(defun height (size) (rest size))
-(defun size+ (size w h)
-  (make-size (+ (width size) w)
-			 (+ (height size) h)))
+(defclass thing ()
+  ((content
+	 :initarg :content
+	 :initform (error "content is required"))
+   (x
+	 :initarg :x
+	 :initform nil)
+   (y
+	 :initarg :y
+	 :initform nil)
+   (width
+	 :initform nil)
+   (height
+	 :initform nil)
+   (vertical
+	 :initarg :vertical
+	 :initform nil)
+   (id
+	 :initform (incf *thing-id*))))
 
-(defun draw-text (text x y &optional (font-size 20) (color raylib:+black+)) ; => size
-  (raylib:draw-text text x y font-size color)
-  (make-size (raylib:measure-text text font-size) font-size))
+(defmethod initialize-instance :after ((th thing) &key)
+  (let ((size (calc-thing-size (slot-value th 'content)
+							   (slot-value th 'vertical))))
+	(setf (slot-value th 'width) (width size))
+	(setf (slot-value th 'height) (height size))))
+
+(defmethod make-thing (content &optional x y vertical)
+  (make-instance 'thing :content content :x x :y y :vertical vertical))
+
+(defun thing= (t1 t2)
+  (= (slot-value t1 'id)
+	 (slot-value t2 'id)))
+
+(defun size-pack (width1 height1 width2 height2 vertical)
+  (if vertical
+	(cons (max width1 width2)
+		  (+ height1 height2))
+	(cons (+ width1 width2)
+		  (max height1 height2))))
+
+;;; A thing's size depends on its children
+;;; recursive
+(defun calc-thing-size (content vertical) ; => size
+  (cond ((null content)
+		 (if vertical
+		   (make-size 32 24)
+		   (make-size 24 32)))
+		((atom content)
+		 (cons (raylib:measure-text (prin1-to-string content) *font-size*)
+			   *font-size*))
+		(t ; list (recursive case)
+		  (size-pack (calc-thing-size (first content) vertical)
+					 (calc-thing-size (rest content) vertical)
+					 vertical))))
+
+;;; A thing's position depends on its parents
+;;; x,y = parent-x, parent-y
+;;; this traverses through children and sets their positions
+;;; recursive
+(defun update-thing-position (thing x y) ; => thing
+  (let ((x (or x (slot-value thing 'x)))
+		(y (or y (slot-value thing 'y))))
+	(setf (slot-value thing 'x) x)
+	(setf (slot-value thing 'y) y)
+	(let ((content (slot-value thing 'content)))
+	(if (and (listp content) content)
+	  (let ((child (first content)))
+		(update-thing-position child x y)
+		(if (slot-value thing 'vertical)
+		  (update-thing-position (rest content) x (+ y (slot-value child 'height)))
+		  (update-thing-position (rest content) (+ x (slot-value child 'width) y))))))))
+
+(defun draw-text (text x y)
+  (raylib:draw-text text x y *font-size* raylib:+black+))
+
+(defun draw-atom (item x y)
+  (draw-text (prin1-to-string item) x y))
 
 (defun draw-nil (x y width height)
   (raylib:draw-rectangle-lines x y width height raylib:+black+)
-  (raylib:draw-line (+ x width) y x (+ y height) raylib:+black+)
-  (make-size width height))
+  (raylib:draw-line (+ x width) y
+					x (+ y height)
+					raylib:+black+))
 
-;; note: padding in this case means the padding between elements
-(defun draw-list (lst x y vertical padding) ; => size
+;;; draw-thing and draw-list are mutually recursive
+(defun draw-list (lst x y vertical)
   (if lst
-	(let ((thing-size (draw-thing (first lst) x y)))
+	(progn
+	  (if (atom (first lst))
+		(draw-atom (first lst) x y)
+		(draw-thing (first lst) x y))
 	  (if vertical
-		(let ((rest-size
-				(draw-list (rest lst) x (+ y (height thing-size) padding) vertical padding)))
-		  (make-size (max (width thing-size) (width rest-size))
-					 (+ (height thing-size) (height rest-size))))
-		(let ((rest-size
-				(draw-list (rest lst) (+ x (width thing-size) padding) y vertical padding)))
-		  (make-size (+ (width thing-size) (width rest-size))
-					 (max (height thing-size) (height rest-size))))))
+		(draw-list (rest lst) x (+ y (height thing-size)) vertical)
+		(draw-list (rest lst) (+ x (width thing-size)) y vertical)))
 	(if vertical
-	  (size+ (draw-nil x y 32 24) 0 (* 2 padding))
-	  (size+ (draw-nil x y 24 32) (* 2 padding) 0))))
+	  (draw-nil x y 32 24)
+	  (draw-nil x y 24 32))))
 
-(defun draw-atom (item x y)
-  (when (not (or (numberp item)
-				 (symbolp item)
-				 (stringp item)))
-	(error "unknown item: ~s" item))
-  (draw-text (prin1-to-string item) x y))
+;;; draw-thing and draw-list are mutually recursive
+(defun draw-thing (thing &optional (x0 0) (y0 0))
+  (let* ((content (slot-value thing 'content))
+		 (tx (or (slot-value thing 'x) 0))
+		 (ty (or (slot-value thing 'y) 0)))
+	(if (listp content)
+	  (draw-list content (+ tx x0) (+ ty y0) (slot-value thing 'vertical) pad-list)
+	  (draw-atom content (+ tx x0) (+ ty y0)))
+	(raylib:draw-rectangle-lines (+ tx x0) (+ ty y0)
+								 (slot-value thing 'width)
+								 (slot-value thing 'height)
+								 raylib:+black+)))
 
-(defun update-thing (thing &optional (x0 0) (y0 0))
-  (if (or (null (thing-x thing))
-		  (null (thing-y thing)))
-	(thing-update-pos thing x0 y0))
-  (let ((content (thing-content thing)))
-	(when (listp content)
-	  (dolist (child content) ; remember, `dolist' on nil does nothing
-		(update-thing child (thing-x thing) (thing-y thing))))))
+;;; rectangle contains point
+(defun rectangle-contains? (rx ry width height px py)
+  (and (<= rx px (+ rx width))
+	   (<= ry py (+ ry height))))
 
-(defun draw-thing (thing &optional (x0 0) (y0 0) (outline-color raylib:+black+))
-  (let* ((margin-x 8)
-		 (margin-y 8)
-		 (pad-list 5)
-		 (content (thing-content thing))
-		 (tx (or (thing-x thing) 0))
-		 (ty (or (thing-y thing) 0))
-		 (size (if (listp content)
-				 (draw-list content (+ tx margin-x x0) (+ ty margin-y y0) (thing-vertical thing) pad-list)
-				 (draw-atom content (+ tx margin-x x0) (+ ty margin-y y0))))
-		 (outline (size+ size (* 2 margin-x) (* 2 margin-y))))
-
-	;; not related to drawing
-	(thing-update-size thing (width outline) (height outline))
-
-	(raylib:draw-rectangle-lines (+ tx x0) (+ ty y0) (width outline) (height outline) outline-color)
-	outline))
-
-(defun intersects (thing x y)
-  (if (and (thing-width thing)
-		   (thing-height thing))
-	(and (< (thing-x thing) x (+ (thing-x thing) (thing-width thing)))
-		 (< (thing-y thing) y (+ (thing-y thing) (thing-height thing))))))
+;;; thing contains point
+(defun thing-contains? (thing px py)
+  (rectangle-contains? (slot-value thing 'x)
+					   (slot-value thing 'y)
+					   (slot-value thing 'width)
+					   (slot-value thing 'height)
+					   px py))
 
 ;;;; recursively find the deepest nested thing that contains a point
 (defun find-thing-at (things x y &optional parent) ; => (thing . parent)
@@ -105,8 +134,8 @@
   (let ((thing1 (if things (first things))))
 	(cond ((not things)
 		   (cons nil parent))
-		  ((intersects thing1 x y)
-		   (let ((content (thing-content thing1)))
+		  ((thing-contains? thing1 x y)
+		   (let ((content (slot-value thing1 'content)))
 			 (cond ((or (atom content)
 						(null content))
 					(cons thing1 parent))
@@ -115,10 +144,10 @@
 		  (t
 			(find-thing-at (rest things) x y parent)))))
 
-(defparameter *things* (list (make-thing '() 20 20 t)
-							 (make-thing 5 40 20 t)
-							 (make-thing (list (make-thing 'a)
-											   (make-thing 'b)) 80 20)))
+(defparameter *things* (list (make-thing 5 20 20)))
+(print *things*)
+(terpri)
+
 (defparameter *grab-thing* nil) ; thing grabbed by mouse
 (defparameter *grab-x* nil)     ; x offset of mouse within grabbed thing
 (defparameter *grab-y* nil)     ; y offset of mouse within grabbed thing
@@ -147,25 +176,21 @@
 									 *things*)))
 					(when found
 					  (setf *grab-thing* found)
-					  (setf *grab-x* (- mx (thing-x *grab-thing*)))
-					  (setf *grab-y* (- my (thing-y *grab-thing*)))
+					  (setf *grab-x* (- mx (slot-value *grab-thing* 'x)))
+					  (setf *grab-y* (- my (slot-value *grab-thing* 'y)))
 					  ;; Remove the grabbed thing from the respective parent
 					  (let ((parent-without (remove *grab-thing* parent :test #'thing=)))
 						;; Note: this is probably not how your supposed to replace the list contents of a variable!
 						;; I just want to change the value that `parent' refers to.
 						(rplaca parent (first parent-without))
 						(rplacd parent (rest parent-without)))
-					  (thing-update-pos *grab-thing* nil nil)))))
-		  (format t "~&number of *thing*s = ~a" (length *things*)))
+					  (update-thing-position *grab-thing* mx my))))))
 
 	  (raylib:with-drawing
 		(raylib:clear-background raylib:+raywhite+)
 
 		(dolist (thing *things*)
-		  (update-thing thing))
-
-		(dolist (thing *things*)
 		  (draw-thing thing))
 
 		(when *grab-thing*
-			(draw-thing *grab-thing* (- mx *grab-x*) (- my *grab-y*))))))))
+		  (draw-thing *grab-thing* (- mx *grab-x*) (- my *grab-y*))))))))
