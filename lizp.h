18f9a4f91d99c8c6f9b540ce6ed4c357d8d3959f
Izak Halseide <ihalseide@pm.me>
2022-02-23 11:54:37 -0700

    Reorganize header and make env_setup_fn visible

--- lizp.h
+++ lizp.h
@@ -3,24 +3,17 @@
 
 #include "cell.h"
 
-Cell *eval_each (Cell *list, Cell *env);
+void rep(const char *start, int length, Cell *env);
+Cell *init(int ncells);
 
-Cell *eval_ast (Cell *ast, Cell *env);
+Cell *READ(const char *start, int length);
+Cell *EVAL(Cell *ast, Cell *env);
+void PRINT(Cell *expr);
 
-int eval_special (Cell *head, Cell *ast, Cell *env, Cell **ast_out, Cell **env_out);
-
-void apply (Cell *fn, Cell *args, Cell *env, Cell **val_out, Cell **env_out);
-
-Cell *READ (const char *start, int length);
-
-Cell *EVAL (Cell *ast, Cell *env);
-
-void PRINT (Cell *expr);
-
-void rep (const char *start, int length, Cell *env);
-
-void env_set_native_fn (Cell *env, const char *name, int n_params, Native_fn func);
-
-Cell *init (int ncells);
+Cell *eval_each(Cell *list, Cell *env);
+Cell *eval_ast(Cell *ast, Cell *env);
+void eval_special(Cell *sym, Cell *ast, Cell *env, Cell **val_out, Cell **env_out);
+void apply(Cell *fn, Cell *args, Cell *env, Cell **val_out, Cell **env_out);
+void env_setup_fn(Cell *env, const char *str, Native_fn_t id);
 
 #endif /* _LIZP_H */
