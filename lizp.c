80ab2c471e3c321c5cb8aac97c41ce355498f278
Izak Halseide <ihalseide@pm.me>
2022-03-02 16:26:09 -0700

    Make PRINT able to print either readably or un-readably

--- lizp.c
+++ lizp.c
@@ -1,8 +1,5 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <assert.h>
+#include <stdio.h>
 #include <stdbool.h>
 
 #include "lizp.h"
@@ -28,10 +25,10 @@ Val *EVAL (Val *ast, Val *env)
 	return ast;
 }
 
-void PRINT (Val *expr)
+void PRINT (Val *expr, bool readable)
 {
 	static char buffer[2 * 1024];
-	int p_len = PrintVal(expr, buffer, sizeof(buffer), 1);
+	int p_len = PrintVal(expr, buffer, sizeof(buffer), readable);
 	printf("%.*s\n", p_len, buffer);
 }
 
@@ -40,5 +37,6 @@ void rep (const char *start, int length, Val *env)
 {
 	Val *a = READ(start, length);
 	Val *b = EVAL(a, env);
-	PRINT(b);
+	PRINT(b, 1);
 }
+
--- lizp.h
+++ lizp.h
@@ -1,47 +1,11 @@
 #ifndef _LIZP_H
 
-#include "sequence.h"
 #include "value.h"
 #include <stdbool.h>
 
-// Number of special form symbols
-#define SPECIAL_COUNT 6
-
-typedef enum Native_fn Native_fn_t;
-enum Native_fn
-{
-	FN_INVALID,
-	FN_STR,
-	FN_PR_STR,
-	FN_PRN,
-	FN_PRINTLN,
-	FN_LIST,
-	FN_EVAL,
-	FN_SLURP,
-	FN_READ_STR,
-	FN_EMPTY_P,
-	FN_LIST_P,
-	FN_INT_P,
-	FN_STRING_P,
-	FN_FUNCTION_P,
-	FN_SYMBOL_P,
-	FN_COUNT,
-	FN_EQ,
-	FN_LT,
-	FN_GT,
-	FN_LTE,
-	FN_GTE,
-	FN_ADD,
-	FN_SUB,
-	FN_MUL,
-	FN_DIV,
-	FN_CONCAT,
-	FN_FIRST,
-	FN_REST,
-};
 Val *READ(const char *start, int length);
 Val *EVAL(Val *ast, Val *env);
-void PRINT(Val *expr);
+void PRINT(Val *expr, bool readable);
 void rep(const char *start, int length, Val *env);
 
 #endif /* _LIZP_H */
