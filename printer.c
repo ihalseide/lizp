e1b488c4c6b1be5f7c54b3135062f115c7e84279
Izak Halseide <ihalseide@pm.me>
2022-03-02 17:05:38 -0700

    Add test for ValueToDigit function

--- printer.c
+++ printer.c
@@ -275,8 +275,35 @@ int PrintVal(Val *p, char *out, int length, int readable)
 	}
 }
 
+void ValueToDigitTest(void)
+{
+	assert(ValueToDigit(1, false) == '1');
+	assert(ValueToDigit(1, true) == '1');
+
+	assert(ValueToDigit(9, true) == '9');
+	assert(ValueToDigit(9, false) == '9');
+
+	assert(ValueToDigit(10, true) == 'A');
+	assert(ValueToDigit(10, false) == 'a');
+
+	assert(ValueToDigit(15, true) == 'F');
+	assert(ValueToDigit(15, false) == 'f');
+
+	assert(ValueToDigit(16, true) == 'G');
+	assert(ValueToDigit(16, false) == 'g');
+
+	assert(ValueToDigit(35, true) == 'Z');
+	assert(ValueToDigit(35, false) == 'z');
+
+	assert(ValueToDigit(36, true) == '?');
+	assert(ValueToDigit(36, false) == '?');
+
+	assert(ValueToDigit(-1, true) == '?');
+	assert(ValueToDigit(-1, false) == '?');
+}
+
 void PrinterTest(void)
 {
-	assert(0 && "not implemented yet");
+	ValueToDigitTest();
 }
 
